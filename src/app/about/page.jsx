0c0a663683a9d3554f0d89a696babcad7ff99a61
Teddy-FN <tedd.ferdy@gmail.com>
2024-05-22 10:43:31 +0700

    About Page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 flex flex-col gap-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>Welcome to Application IMDB Clone</p>
+      <p>Our App Build Using Next JS 14</p>
+    </div>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl font-medium text-amber-600">Home</h1>
+    </div>
+  );
+}
