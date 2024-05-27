97e41ca77218a0e5e1b0129a8aef70d378cb12f9
Teddy-FN <tedd.ferdy@gmail.com>
2024-05-27 21:26:30 +0700

    Add Loading Fetching Data

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center items-center">
+      <img src="loading.svg" alt="loading" />
+    </div>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,17 @@ import Result from "@/components/Result";
 
 export default async function Home({ searchParams }) {
   const params = searchParams.genre || "fetchTrending";
-  const res = await fetch(
-    `https://api.themoviedb.org/3${
-      params === "fetchTrending" ? "/trending/all/day" : "/movie/top_rated"
-    }?api_key=${API_KEY}&language=en-US&page=1`
-  );
+  const res = await new Promise((resolve) => {
+    setTimeout(async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3${
+          params === "fetchTrending" ? "/trending/all/day" : "/movie/top_rated"
+        }?api_key=${API_KEY}&language=en-US&page=1`,
+        { next: { revalidate: 10 } }
+      );
+      resolve(response);
+    }, 2000);
+  });
 
   const data = await res.json();
 
